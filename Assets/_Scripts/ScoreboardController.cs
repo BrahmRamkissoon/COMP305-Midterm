2894c29c63fd97ff9293d36e2b99fe7c6f86239b
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-27 21:19:04 -0400

    attached scripts, applied prefab changes, added Scoreboard

---
+++ Assets/_Scripts/ScoreboardController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreboardController : MonoBehaviour {
+    //  PUBLIC iNSTANCE VARIABLES +++++++++++++++++
+    public Text scoreLabel;
+    public Text livesLabel;
+
+
+    private int _score = 0;
+    private int _lives = -5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //  PRIVATE METHODS
+    private void _ScoreUpdate ()
+    {
+        this.scoreLabel.text = "Score: " + this._score;
+        this.livesLabel.text = "Lives: " + this._lives;
+    }
+}
